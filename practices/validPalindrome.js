6b7d734f12ea0def425301dc3f682a707cb8a412
aaronfeingold <ajfeingold88@gmail.com>
2023-11-01 10:37:33 -0400

    using replace instead of match, and replace anything that is not a to z or 0 to 9

--- practices/validPalindrome.js
+++ practices/validPalindrome.js
@@ -6,23 +6,9 @@
 // run time = 78ms, beats 12.97%
 // memory = 53.98, beats 5.08 %
 var isPalindrome = function(s) {
-    const string = s.trim().toLowerCase()
+    const str = s.replace(/[^a-z0-9]/gi, "").toLowerCase()
 
-    const arr = []
-    for (const char of string) {
-        if (char.match(/[a-z0-9]/i)) {
-            arr.push(char)
-        }
-    }
-
-    const fwd = arr.join("")
-    const rvs = [ ...arr ].reverse().join("")
-
-    if (fwd === rvs) {
-        return true
-    }
-
-    return false
+    return str.split("").reverse().join("") === str;
 };
 
 // should be false...but why?
