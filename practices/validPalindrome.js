a04f36d87d7c5091b41746011c4ce390969165f3
aaronfeingold <ajfeingold88@gmail.com>
2023-11-01 10:05:21 -0400

    new problem solved

---
+++ practices/validPalindrome.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+
+var isPalindrome = function(s) {
+    const string = s.trim().toLowerCase()
+
+    const arr = []
+    for (const char of string) {
+        if (char.match(/[a-z]/i)) {
+            arr.push(char)
+        }
+    }
+
+    const fwd = arr.join("")
+    const rvs = arr.reverse().join("")
+
+    if (fwd === rvs) {
+        return true
+    }
+
+    return false
+};
