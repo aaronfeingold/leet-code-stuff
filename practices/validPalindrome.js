761e80961994ccdab8d104ac04954d763cc73b79
aaronfeingold <ajfeingold88@gmail.com>
2023-11-01 10:25:25 -0400

    these version stinks

--- practices/validPalindrome.js
+++ practices/validPalindrome.js
@@ -3,12 +3,14 @@
  * @return {boolean}
  */
 
+// run time = 78ms, beats 12.97%
+// memory = 53.98, beats 5.08 %
 var isPalindrome = function(s) {
     const string = s.trim().toLowerCase()
 
     const arr = []
     for (const char of string) {
-        if (char.match(/[a-z]/i)) {
+        if (char.match(/[a-z0-9]/i)) {
             arr.push(char)
         }
     }
