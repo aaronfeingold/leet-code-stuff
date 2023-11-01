304cbd61fb70b931c6deebdc4230668475507a63
aaronfeingold <ajfeingold88@gmail.com>
2023-11-01 11:00:01 -0400

    comments on the time and space of this fn

--- practices/validPalindrome.js
+++ practices/validPalindrome.js
@@ -4,7 +4,9 @@
  */
 
 // run time = 52ms, beats 94.59%
+// time complexity is O(n) since we predominantly linear scan the s
 // memory = 46.58, beats 40.67 %
+// space complexity if O(n) since the str and the reverse str all depend on the length of s
 var isPalindrome = function(s) {
     const str = s.replace(/[^a-z0-9]/gi, "").toLowerCase()
 
