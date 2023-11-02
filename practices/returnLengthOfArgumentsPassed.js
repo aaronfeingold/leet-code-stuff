6f95c488cbf55d943915d97a929f73bcf1a8c258
aaronfeingold <ajfeingold88@gmail.com>
2023-11-02 11:14:00 -0400

    this is O(1) because we no longer create a new array, which is O(n)

--- practices/returnLengthOfArgumentsPassed.js
+++ practices/returnLengthOfArgumentsPassed.js
@@ -2,8 +2,17 @@
  * @param {...(null|boolean|number|string|Array|Object)} args
  * @return {number}
  */
+/**
+Runtime
+- 51ms
+- Beats 55.46%of users with JavaScript
+Memory
+- 41.98MB
+- Beats 40.73%of users with JavaScript
+ */
+// Time and Space are both O(n) since it we traverse the arguments given
 var argumentsLength = function(...args) {
-    return [...args].length
+    return args.length
 };
 
 /**
