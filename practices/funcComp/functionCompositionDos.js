ded8f248a1435d283c70a049d995dd7c4b1343b2
aaronfeingold <ajfeingold88@gmail.com>
2023-11-02 12:12:49 -0400

    cleanup funcCompDos

--- practices/funcComp/functionCompositionDos.js
+++ practices/funcComp/functionCompositionDos.js
@@ -24,12 +24,8 @@ var compose = function(functions) {
     return function (x) {
       return functions.reverse().reduce((acc, fn) => fn(acc), x);
     };
-  };
+};
 
-  /**
-   * const fn = compose([x => x + 1, x => 2 * x])
-   * fn(4) // 9
-   */
 
 /**
  * const fn = compose([x => x + 1, x => 2 * x])
