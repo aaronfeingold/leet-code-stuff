20f65460047cd8b905ced03db54313421aef6ce1
aaronfeingold <ajfeingold88@gmail.com>
2023-11-16 18:18:26 -0500

    a little diff, if x is less than 1 too, just return x. plus math.floor will save us memory since we storing fewer integers to memory...maybe this helps idk really

--- practices/mySqrt.js
+++ practices/mySqrt.js
@@ -14,20 +14,26 @@ Memory
  */
 var mySqrt = function(x) {
     // define some boundaries
-    if (x === 1) {
+    if (x <= 1) {
         return x
     }
     // a square root of any number cannot be greater than the number divided in half
     // this eliminates half the numbers we have to iterate over
-    let upperBound = x/2 + 1
+    let upperBound = Math.floor(x/2) + 1
 
     let lowerBound = 0
 
     // binary search at O(log(n))
     while (lowerBound <= upperBound) {
+        // the square root has to be somewhere between the lower and upper bounds
+        // so start at the midpoint
         let midPoint = Math.floor((lowerBound + upperBound) /2)
         let squared = midPoint * midPoint
-        if (squared <= x) {
+        // and if it is less than x, increase the the lower bound
+        // and repeat till lower exceeds upper, then return the upper
+        if (squared === x){
+            return midPoint
+        } else if(squared < x) {
             lowerBound = midPoint + 1
         } else {
             upperBound = midPoint - 1
