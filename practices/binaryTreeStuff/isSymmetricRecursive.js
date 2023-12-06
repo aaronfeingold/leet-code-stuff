56a3efaa55c968e09d1eb36eb74318d1a6e0e8fc
aaronfeingold <ajfeingold88@gmail.com>
2023-12-06 13:26:29 -0500

    expecting 5 on the height

--- practices/binaryTreeStuff/isSymmetricRecursive.js
+++ practices/binaryTreeStuff/isSymmetricRecursive.js
@@ -6,6 +6,13 @@
  *     this.right = (right===undefined ? null : right)
  * }
  */
+
+function TreeNode (val = 0, left = null, right = null) {
+    this.val = val;
+    this.left = left;
+    this.right = right;
+};
+
 /**
  * @param {TreeNode} root
  * @return {boolean}
@@ -21,3 +28,27 @@ var helper = (t1, t2) => {
 
     return helper(t1.left, t2.right) && helper(t1.right, t2.left)
 }
+
+// get the height of BT
+var getHeight = (node) => {
+    if (!node) return 0;
+
+    var lH = getHeight(node.left);
+    var RH = getHeight(node.right);
+
+    return Math.max(lH, RH) + 1;
+}
+
+const newRoot = new TreeNode(1);
+const nodeOne = new TreeNode(2);
+const nodeTwo = new TreeNode(3);
+newRoot.left = nodeOne;
+newRoot.right = nodeTwo
+nodeOne.left = new TreeNode(4);
+nodeOne.right = new TreeNode(5)
+nodeTwo.right = new TreeNode(6)
+nodeTwo.right.right = new TreeNode(7);
+nodeTwo.right.right.right = new TreeNode(8);
+
+// expect 5
+console.log(getHeight(newRoot))
