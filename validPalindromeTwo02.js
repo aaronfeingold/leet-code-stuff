6e30945cc5c6139dd246ef8bbd4fa00f21747228
aaronfeingold <ajfeingold88@gmail.com>
2023-12-14 14:13:58 -0500

    idk i think this should work...;

---
+++ validPalindromeTwo02.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+// two pointer technique
+var validPalindrome = function(s, deletions = false) {
+    let i = 0;
+    let j = s.length - 1;
+
+    while (i < j) {
+        if (s[i] !== s[j]) {
+            if (deletions) {
+                return false;
+            }
+            return validPalindrome(s.slice(i+1, j), true) || validPalindrome(s.slice(i, j-1), true);
+        }
+
+        i++;
+        j--;
+
+    }
+
+    return true;
+};
+
+console.log(validPalindrome("ebcbbececabbacecbbcbe"))
