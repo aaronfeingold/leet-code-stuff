91b76970b86346e24d9356dc991fb1dee9a1cd9f
aaronfeingold <ajfeingold88@gmail.com>
2024-06-10 16:02:51 -0500

    palindromNumber.js: two pointer technique, its pretty fast IMO

--- practices/palindromeNumber.js
+++ practices/palindromeNumber.js
@@ -9,3 +9,29 @@ const isPalindrome = function(x) {
 };
 
 console.log(isPalindrome(1000021))
+
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindromeTwoPointers = function(x) {
+    // two pointer technique
+    // step one, convert number to array
+    // step two, compare pointers and move them along
+    const arr = Array.from(String(x));
+    let i = 0;
+    let j = arr.length - 1;
+
+    while (i < j) {
+        if (arr[i] === arr[j]) {
+            // loop housekeeping
+            i++;
+            j--;
+            continue;
+        } else {
+            return false;
+        }
+    }
+
+    return true;
+}
