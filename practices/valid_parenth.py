f9e8290a37ca6fdd020abd532073e0881405e434
aaronfeingold <ajfeingold88@gmail.com>
2025-05-28 14:49:07 -0500

    short circuit

--- practices/valid_parenth.py
+++ practices/valid_parenth.py
@@ -1,18 +1,15 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
-        map = {
-            '[':']',
-            '{':'}',
-            '(':')'
-        }
+        pairs = {')': '(', ']': '[', '}': '{'}
 
         for char in s:
-            if char in map.keys():
+            if char in pairs.values():
                 stack.append(char)
-            else:
-                popped = stack.pop()
-                if map[popped] != char:
+            elif char in pairs:
+                if not stack or stack.pop() != pairs[char]: # calling pop on empty list will raise an error, but we short circuit it
                     return False
+            else:
+                return False  # invalid character
 
-        return len(stack) == 0
+        return not stack
